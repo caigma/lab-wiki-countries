f2f3a512e4d30617eb7f65def280e6b79141e086
caigma <caigma@gmail.com>
2019-02-20 23:12:33 +0100

    terminado

---
+++ starter-code/src/components/AllCountries.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import countries from '../countries.json';
+
+export default class AllCountries extends Component {
+	state = {
+		countries: [ ...countries ]
+	};
+
+	render() {
+		return (
+			<div className="allcountries">
+				{this.state.countries.map((country) => {
+					return (
+						<NavLink
+							className="list-group-item list-group-item-action"
+							to={`/${country.cca3}`}
+							key={country.cca3}
+						>
+							{country.flag} {country.name.official}
+						</NavLink>
+					);
+				})}
+			</div>
+		);
+	}
+}
